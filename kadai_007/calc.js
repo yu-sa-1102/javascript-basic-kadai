ceca70d18ee41297737556bb505cb30201c08919
yu-sa-1102 <spica-yihsa3@i.softbank.jp>
2024-12-22 13:59:38 +0900

    kadai_007のJavaScriptを更新

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
-let num = 8;
+let num = 30;
 
-if (num % 3 == 0) {
+if (num % 3 == 0 && num % 5 ==0) {
+    console.log('3と5の倍数です');
+}
+
+else if (num % 3 == 0 ) {
     console.log('3の倍数です');
 }
 
 else if (num % 5 ==0) {
     console.log('5の倍数です');
-}
-
-else if (num % 3 == 0 && num % 5 ==0) {
-    console.log('3と5の倍数です');
 }
 
 else {
