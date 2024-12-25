b88948794a6f76b04b72f80c6ed20b755aedcc20
yu-sa-1102 <spica-yihsa3@i.softbank.jp>
2024-12-25 21:01:04 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+const today = new Date();
+
+const year = today.getFullYear();
+
+const month = today.getMonth()+1;
+
+const day = today.getDate();
+
+const putDate = () => {
+    console.log(year+'年'+month+'月'+day+'日');
+}
+
+putDate();
